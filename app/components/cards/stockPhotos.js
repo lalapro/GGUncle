b154ec7b164298a694fee15a4048520c8d696de4
Jonathan Yuen <jonathanyuenby@gmail.com>
2018-04-19 14:43:07 -0400

    Add files via upload

---
+++ app/components/cards/stockPhotos.js
@@ -0,0 +1,18 @@
+let stockPhotos = {
+  'Bowls': require('./stockPhotos/Soup.jpg'),
+  'All Day Breakfast': require('./stockPhotos/Breakfast.jpg'),
+  'Build-Your-Own Plate': require('./stockPhotos/BuildPlate.jpg'),
+  'Cold Subs': require('./stockPhotos/ColdSubs.jpg'),
+  'Hot Sandwiches': require('./stockPhotos/HotSandwiches.jpg'),
+  'Pasta': require('./stockPhotos/Pasta.jpeg'),
+  'Pizza': require('./stockPhotos/Pizza.jpeg'),
+  'Pub Food': require('./stockPhotos/PubFood.jpg'),
+  'Chopped Salads': require('./stockPhotos/Salads.jpeg'),
+  'Sauces': require('./stockPhotos/Sauces.jpeg'),
+  'Soup': require('./stockPhotos/Soup.jpg'),
+  'Treats': require('./stockPhotos/Treats.jpg'),
+  'Wraps': require('./stockPhotos/Wraps.jpeg'),
+}
+
+
+export default stockPhotos;
